d9a0c35be5826b4d112c9f2616a7403fe6e8fa66
SyouyaNiwa <109267191+ssyniwa@users.noreply.github.com>
2026-07-20 09:32:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import random
-from data import INGREDIENTS, RACES, PREFERENCES, RECIPES
+from data import  RACES, PREFERENCES, RECIPES
 
 
 st.set_page_config(page_title="ファンタジーキッチン")
